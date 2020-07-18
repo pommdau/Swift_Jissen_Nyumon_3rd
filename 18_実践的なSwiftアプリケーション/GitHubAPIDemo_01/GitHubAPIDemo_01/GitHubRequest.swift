1fe64e4d40a5055de90f276315b6985bf9206b15
ikeh1024 <pondau1024@gmail.com>
2020-07-18 12:49:57 +0900

    P863まで

--- 18_実践的なSwiftアプリケーション/GitHubAPIDemo_01/GitHubAPIDemo_01/GitHubRequest.swift
+++ 18_実践的なSwiftアプリケーション/GitHubAPIDemo_01/GitHubAPIDemo_01/GitHubRequest.swift
@@ -23,4 +23,35 @@ public extension GitHubRequest {
     var baseURL: URL {  // デフォルト実装
         return URL(string: "https://api.github.com")!
     }
+    
+    func buildURLRequest() -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        
+        switch method {
+        case .get:
+            components?.queryItems = queryItems
+        default:
+            fatalError("Unsupported method \(method)")
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.url = components?.url
+        urlRequest.httpMethod = method.rawValue
+        
+        return urlRequest
+    }
+    
+    // URLSessionを通じてサーバから受け取ったData型 + HTTPURLResponse型の数値 = レスポンスの型を表すRespnseの値
+    func response(from data: Data,
+                  urlResponse: HTTPURLResponse) throws -> Response {
+
+        let decoder = JSONDecoder()
+        if (200..<300).contains(urlResponse.statusCode) {
+            return try decoder.decode(Response.self, from: data)
+        } else {
+            throw try decoder.decode(GitHubAPIError.self, from: data)
+        }        
+    }
+    
 }
