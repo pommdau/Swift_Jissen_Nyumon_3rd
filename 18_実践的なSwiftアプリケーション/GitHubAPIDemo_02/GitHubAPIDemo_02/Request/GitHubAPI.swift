3581f4ce772f3b941e84fce3cff8ea1d75207f59
ikeh1024 <pondau1024@gmail.com>
2020-07-24 01:54:02 +0900

    リクエストのモデル作成まで

---
+++ 18_実践的なSwiftアプリケーション/GitHubAPIDemo_02/GitHubAPIDemo_02/Request/GitHubAPI.swift
@@ -0,0 +1,57 @@
+//
+//  GitHubAPI.swift
+//  GitHubAPIDemo_02
+//
+//  Created by HIROKI IKEUCHI on 2020/07/24.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import Foundation
+
+public final class GitHubAPI {
+    public struct SearchRepositories : GitHubRequest {
+        public let keyword: String
+        
+        // GitHubRewuestが要求する連想型
+        public typealias Response = SearchResponse<Repository>
+        
+        public var method: HTTPMethod {
+            return .get
+        }
+        
+        public var path: String {
+            return "/search/repositories"
+        }
+        
+        public var queryItems: [URLQueryItem] {
+            return [URLQueryItem(name: "q", value: keyword)]
+        }
+        
+        public var body: Encodable?
+        
+//        public init(keyword: String) {
+//            self.keyword = keyword
+//        }
+    }
+    
+    public struct SearchUsers : GitHubRequest {
+        public let keyword: String
+        
+        // GitHubRewuestが要求する連想型
+        public typealias Response = SearchResponse<User>
+        
+        public var method: HTTPMethod {
+            return .get
+        }
+        
+        public var path: String {
+            return "/search/users"
+        }
+        
+        public var queryItems: [URLQueryItem] {
+            return [URLQueryItem(name: "q", value: keyword)]
+        }
+        
+        public var body: Encodable?
+    }
+}
