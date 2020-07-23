0a59b6fe39d0ed99eb13b794671a0a65e459277e
ikeh1024 <pondau1024@gmail.com>
2020-07-24 03:46:09 +0900

    最後まで

---
+++ 18_実践的なSwiftアプリケーション/GitHubAPIDemo_02/GitHubAPIDemo_02/Request/GitHubClient.swift
@@ -0,0 +1,39 @@
+//
+//  GitHubClient.swift
+//  GitHubAPIDemo_02
+//
+//  Created by HIROKI IKEUCHI on 2020/07/24.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import Foundation
+
+public class GitHubClient {
+    private let httpClient: HTTPClient  // HTTPClientプロトコルに準拠した型 e.g. URLSession.sharedとか
+    
+    public init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+    
+    public func send<Request : GitHubRequest> (
+        request: Request,
+        completion: @escaping (Result<Request.Response, GitHubClientError>) -> Void) {
+        let urlRequest = request.buildURLRequest()
+        
+        httpClient.sendRequest(urlRequest) { result in
+            switch result {
+            case .success(let data, let urlResponse):
+                do {
+                    let response = try request.response(from: data, urlResponse: urlResponse)
+                    completion(Result.success(response))
+                } catch let error as GitHubAPIError {
+                    completion(Result.failure(.apiError(error)))
+                } catch {
+                    completion(.failure(.responseParseError(error)))
+                }
+            case .failure(let error):
+                completion(.failure(.connectionError(error)))
+            }
+        }
+    }
+}
---
+++ 18_実践的なSwiftアプリケーション/GitHubAPIDemo_02/GitHubAPIDemo_02/Request/HTTPClient.swift
@@ -0,0 +1,34 @@
+//
+//  HTTPClient.swift
+//  GitHubAPIDemo_02
+//
+//  Created by HIROKI IKEUCHI on 2020/07/24.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import Foundation
+
+// HTTPクライアントの最小限の機能をプロトコルで定義する
+public protocol HTTPClient {
+    func sendRequest(_ urlRequest: URLRequest,
+                     completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void)
+}
+
+extension URLSession : HTTPClient {
+    public func sendRequest(_ urlRequest: URLRequest,
+                            completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+
+        let task = dataTask(with: urlRequest) { data, urlResponse, error in
+            switch (data, urlResponse, error) {
+            case (_, _, let error?):
+                completion(Result.failure(error))
+            case (let data?, let urlResponse as HTTPURLResponse, _):
+                completion(Result.success((data, urlResponse)))
+            default:
+                fatalError("invalid response combination \(String(describing: (data, urlResponse, error))).")
+            }
+        }
+        
+        task.resume()
+    }
+}
